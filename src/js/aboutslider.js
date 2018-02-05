2a2855d5552ea2a5d96f20275153224b68aeb5fe
Dan Foy <contact@danfoy.com>
2018-02-05 16:11:09 +0000

    Stop about page from scrolling forever
    The about page scrolled forever because of the image sliding script. The
    script now measures the distance the image needs to travel and sets
    `display: none` when the image is offscreen. This interfered with the
    way the bio text was aligned, so I had to change the positioning for
    elements within `.sectionheader` also.

--- src/js/aboutslider.js
+++ src/js/aboutslider.js
@@ -1,33 +1,65 @@
-// Sync test
-
+/**
+ * [PictureSlider Element to be slider-ed]
+ * @param {string} target class assigned to target image
+ */
 function PictureSlider(target) {
-    this.moveTarget = document.getElementsByClassName(target)[0];
+    this.moveWrapper = document.getElementsByClassName(target);
+    this.moveTarget = this.moveWrapper[0];
     this.moveTargetPosition = this.moveTarget.getBoundingClientRect();
     this.bodyPosition = document.body.getBoundingClientRect();
     this.horizontalOffset = this.moveTargetPosition.left - this.bodyPosition.left;
     this.verticalOffset = this.moveTargetPosition.top - this.bodyPosition.top;
 }
 
+/**
+ * [beginTracking begin tracking for scrolling]
+ */
 PictureSlider.prototype.beginTracking = function () {
-    this.moveTarget.style.position = "relative";
+    
     window.addEventListener('scroll', function () {
+
+        
+        var scrollPosition = window.pageYOffset;
+        var distanceToEdge = this.moveTargetPosition.right - (window.pageYOffset * 4);
+
         if (window.innerWidth >= 799 && 
             window.innerWidth <= 1440) {
+            
             // Target is landscape tablets and
             // computers  up to 15" mbp
-            var scrollPosition = window.pageYOffset;
+            
+            this.moveTarget.style.position = "absolute";
+            
             this.moveTarget.style.right = ((scrollPosition * 4) + 'px');
             this.moveTarget.style.top = ((scrollPosition * 2) + 'px');
+            
+            if (distanceToEdge <= 0) {
+                this.moveTarget.style.display = 'none';
+            } else {
+                this.moveTarget.style.display = 'inline-block';
+            }
+        
         } else if (window.innerWidth > 1440 &&
                    window.innerWidth <= 1920) {
+
             // Targets computers larger than 15" mbp,
             // TVs and consoles etc., but not larger
             // than 1080p to avoid scrolling issues
-            var scrollPosition = window.pageYOffset;
+            
+            this.moveTarget.style.position = "absolute";
+
             this.moveTarget.style.right = ((scrollPosition * 6) + 'px');
             this.moveTarget.style.top = ((scrollPosition * 2) + 'px');
+
+            if (distanceToEdge <= 0) {
+                this.moveTarget.style.display = 'none';
+            } else {
+                this.moveTarget.style.display = 'inline-block';
+            }
         }
+    
     }.bind(this));
+
 };
 
 
