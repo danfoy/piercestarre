f1fb353ab72d5828c62f7ed18fdd9a93cbb0a5a1
Dan Foy <contact@danfoy.com>
2018-03-19 19:22:30 +0000

    Disable About page animations
    Too distracting at the moment and other priorities ahead of porting this
    to all pages. Left the code in (commented out) for later reference.

--- src/functions.php
+++ src/functions.php
@@ -53,13 +53,15 @@ function enqueue_ps2017_scripts()
         1,
         true
     );
-    wp_register_script(
-        'animate_about',
-        get_template_directory_uri() . '/js/animateabout.js',
-        array(),
-        1,
-        true
-    );
+    // Animations disabled until a later release.
+    // 
+    // wp_register_script(
+    //     'animate_about',
+    //     get_template_directory_uri() . '/js/animateabout.js',
+    //     array(),
+    //     1,
+    //     true
+    // );
     wp_enqueue_script('animate_menu');
     if (is_page('about')) {
         // wp_enqueue_script('about_slider');
