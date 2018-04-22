5acddf989f5d4ae777f46244ab220b31896272f9
Dan Foy <contact@danfoy.com>
2018-04-22 22:41:48 +0100

    Change position of date on CV entries
    Moved to the end because the dates don't line up on the left now that
    the text is centre aligned.

--- src/loop-about.php
+++ src/loop-about.php
@@ -5,7 +5,6 @@ $linkmeta 		= get_post_meta( get_the_ID(), 'link', true);
 
 <li class="cv-item">
     <?php
-    echo '<span class="cv-item-date"><span class="date">' . get_the_date('Y') . '.</span></span>';
     echo '<span class="cv-item-description">';
     if ($linkmeta) {
     	echo '<a href="' . $linkmeta . '" target="_blank">' . get_the_title() . '</a>';
@@ -15,5 +14,6 @@ $linkmeta 		= get_post_meta( get_the_ID(), 'link', true);
     if ( $locationmeta ) {
         echo ' &mdash; <i class="location">' . $locationmeta . "</i>";
     };
+    echo ', <span class="cv-item-date"><span class="date">' . get_the_date('Y') . '.</span></span>';
 	echo '</span>'; ?>
 </li>
