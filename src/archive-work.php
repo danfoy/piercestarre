cb6de5cb3daafda53a7cffb9ef8dba17e541b7c6
Dan Foy <contact@danfoy.com>
2018-10-04 16:25:09 +0100

    Change work archive to flexbox grid w/ placeholder
    The grid is now handled by flexbox and supports placeholders for
    projects which haven't had a featured image assigned. Also added
    comments liberally to aid future changes or refactoring.

--- src/archive-work.php
+++ src/archive-work.php
@@ -8,7 +8,7 @@ echo '<main class="main" role="main">';
 if (have_posts()) {
 
     // Open list of projects
-    echo '<ul class="project-archive-grid">';
+    echo '<ol class="project-archive-grid">';
 
     // For each post...
     while (have_posts()) {
@@ -18,14 +18,20 @@ if (have_posts()) {
         
         // Create a list item for each post
         echo '<li class="project-archive-item">';
-        echo    '<a class="project-archive-item-link" href="' . get_the_permalink . '" title="' . get_the_title() . '">';
-                    the_post_thumbnail('work-thumbnail');
+        echo    '<a class="project-archive-item-link" href="' . get_the_permalink() . '" title="' . get_the_title() . '">';
+        
+        if (has_post_thumbnail()) {
+            the_post_thumbnail('work-thumbnail');
+        } else {
+            echo '<span class="post-thumbnail-placeholder">PS</span>';
+        };
+
         echo    '</a>'; // .project-archive-item-link
         echo '</li>'; // .project-archive-item
     }
     
     // Close the list
-    echo '</ul>'; // .project-archive-grid
+    echo '</ol>'; // .project-archive-grid
     
 } else {
     // Handle display when there are no posts
