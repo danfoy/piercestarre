72c69a2c2e51da6c75ab8fd6fd4a3f3ee9df9725
danfoy <contact@danfoy.com>
2018-11-09 21:31:04 +0000

    Add credit for project images
    Pierce can add credit for the people who document his perfomances via
    two custom post meta fields: image-credit-link and image-credit-name.
    Appears at the bottom of the post meta area, after location.

--- src/php/pages/single-work.php
+++ src/php/pages/single-work.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 get_header();
 
@@ -8,14 +8,16 @@ echo '<main class="main" role="main">';
 // Start the loop, check for posts
 if (have_posts()) {
     while (have_posts()) {
-        
+
         // Setup post
         the_post();
 
         // Set up local variables
-        $meta_location = get_post_meta( get_the_ID(), 'location', true);
-        $content = get_the_content();
-        
+        $meta_location          = get_post_meta( get_the_ID(), 'location',          true);
+        $meta_image_credit_name = get_post_meta( get_the_ID(), 'image-credit-name', true);
+        $meta_image_credit_link = get_post_meta( get_the_ID(), 'image-credit-link', true);
+        $content                = get_the_content();
+
         // Get first image tag in $content and add to array $matches
         preg_match("/<img[^>]+\>/i", $content, $matches);
         // Strip images from $content
@@ -35,10 +37,41 @@ if (have_posts()) {
             ps2017_sidescroll_gallery();
         };
 
-        // Print project deta and location
+        // Print project date, which is set to the post date
         echo '<div class="project-date">'. get_the_time('j F Y') . '</div>';
+
+        // Print the project location from custom post meta, if exists
         if ( $meta_location ) {
-            echo '<div class="project-location">' . $meta_location . "</div>";
+            echo '<div class="project-location">' . $meta_location . '</div>';
+        };
+
+        // Print credit for the project photo if exists, optionally inside a link
+        if ( $meta_image_credit_name ) {
+            echo '<div class="project-image-credit">';
+            echo '<span class="project-image-credit-title">Image: </span>';
+
+            // Create a link if there is one, else wrap in a <span>
+            if ($meta_image_credit_link) {
+                // Add http to the link if ommitted. Avoids WordPress generating a relative URL.
+                if (strpos($meta_image_credit_link, 'http') === false) {
+                    $meta_image_credit_link = 'http://' . $meta_image_credit_link;
+                };
+                echo '<a href="' . $meta_image_credit_link . '" class="project-image-credit-link" target="_blank">';
+            } else {
+                echo '<span class="project-image-credit-name">';
+            };
+
+            // Print the the name we're crediting
+            echo $meta_image_credit_name;
+
+            // Close either the link or the <span>
+            if ($meta_image_credit_link) {
+                echo '</a>'; // .project-image-credit-link
+            } else {
+                echo '</span>'; // .project-image-credit-name
+            };
+
+            echo '</div>'; // .project-image-credit
         };
 
 
